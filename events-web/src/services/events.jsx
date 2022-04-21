a1796b58a2ae8da47a85f22710a6f45e6c87da7d
oleksiuk-ol <oleksiukol@protonmail.ch>
2022-04-21 16:42:11 +0200

    fix: remove console.log

--- events-web/src/services/events.jsx
+++ events-web/src/services/events.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const createEvent = async (eventData) => {
-  console.log("CREATE EVENT DATA ", eventData);
   return axios
     .post(`http://localhost:3001/event/event/createEvent`, {
       ...eventData,
